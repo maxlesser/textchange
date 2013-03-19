8e207d06c41c506c72129c9f8302101d4ee03ce2
Max <wheels@nyc.rr.com>
2013-03-18 23:38:06 -0400

    changing to node

--- User/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    username = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
-
-    def __unicode__(self):
-        return self.username
