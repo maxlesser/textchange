1e116c3bc2f4690d7b4d92713d365b2c38641da7
Max <wheels@nyc.rr.com>
2013-04-03 18:25:56 -0400

    login page

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ var engines = require('consolidate');
 var app = express();
 
 app.use(express.bodyParser()); // definitely use this feature
+app.use(passport.initialize());
+// app.use(passport.session());
 
 //public files like pictures are in public
 app.use('/public', express.static(__dirname + '/public'));
@@ -15,9 +17,34 @@ app.use('/public', express.static(__dirname + '/public'));
 app.engine('html', engines.hogan); // tell Express to run .html files through Hogan
 app.set('views', __dirname + '/templates'); // tell Express where to find templates
 
+
+//passport setup
+var passport = require('passport'), LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+
 //home page response
 app.get('/', function(request, response){
 	response.render('home.html');
 });
+
+//login page
+app.post('/login',
+  passport.authenticate('local', 
+  	{ successRedirect: '/', failureRedirect: '/login' }));
 
 app.listen(8080);
