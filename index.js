a35848b5ce5307bd97ac52bcbe52b8a4b87bc94a
Max <wheels@nyc.rr.com>
2013-04-03 18:57:00 -0400

    small changes

--- index.js
+++ index.js
@@ -7,35 +7,80 @@ var engines = require('consolidate');
 //passport setup
 var passport = require('passport'), LocalStrategy = require('passport-local').Strategy;
 
-//var conn = anyDB.createConnection('sqlite3://chatrooms.db');
-var app = express();
 
-app.use(express.bodyParser()); // definitely use this feature
-app.use(passport.initialize());
-// app.use(passport.session());
+var users = [
+    { id: 1, username: 'max', password: 'lesser', email: 'max@example.com' }
+  , { id: 2, username: 'andy', password: 'chen', email: 'andy@example.com' }
+];
 
-//public files like pictures are in public
-app.use('/public', express.static(__dirname + '/public'));
+function findById(id, fn) {
+  var idx = id - 1;
+  if (users[idx]) {
+    fn(null, users[idx]);
+  } else {
+    fn(new Error('User ' + id + ' does not exist'));
+  }
+}
 
-app.engine('html', engines.hogan); // tell Express to run .html files through Hogan
-app.set('views', __dirname + '/templates'); // tell Express where to find templates
+function findByUsername(username, fn) {
+  for (var i = 0, len = users.length; i < len; i++) {
+    var user = users[i];
+    if (user.username === username) {
+      return fn(null, user);
+    }
+  }
+  return fn(null, null);
+}
 
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  findById(id, function (err, user) {
+    done(err, user);
+  });
+});
 
 
 passport.use(new LocalStrategy(
   function(username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
+    // Find the user by username.  If there is no user with the given
+    // username, or the password is not correct, set the user to `false` to
+    // indicate failure and set a flash message.  Otherwise, return the
+    // authenticated `user`.
+    findByUsername(username, function(err, user) {
       if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
+      if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+      if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
       return done(null, user);
     });
   }
 ));
+
+
+
+//var conn = anyDB.createConnection('sqlite3://chatrooms.db');
+var app = express();
+
+app.configure(function() {
+  app.engine('html', engines.hogan); // tell Express to run .html files through Hogan
+  app.set('views', __dirname + '/templates'); // tell Express where to find templates
+  app.use(express.cookieParser());
+  app.use(express.bodyParser()); // definitely use this feature
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  //public files like pictures are in public
+  app.use('/public', express.static(__dirname + '/public'));
+});
+
 
 
 //home page response
