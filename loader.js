b211080b7881a6ae4c6cacebe8df04829a93ea34
Max <wheels@nyc.rr.com>
2013-04-08 22:55:11 -0400

    loader

--- loader.js
+++ loader.js
@@ -16,12 +16,15 @@ conn.query('CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT,
 
 conn.query('CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT,email TEXT, password TEXT, name TEXT)');
 
+conn.query('CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT,author TEXT,class TEXT, seller TEXT, image TEXT, description TEXT, time INTEGER)');
 
+var d = new Date();
 
 for (var i = 0; i < 100; i++)
 {
 
 conn.query('INSERT INTO books (title, author, class, seller, image, time) VALUES (\'Test' + i + '\', \'Johan\', \'CSCI019\', \'Bob\', \'path\', 100)');
+conn.query('INSERT INTO books (title, author, class, seller, image,description, time) VALUES (\'Test' + i + '\', \'Johan\', \'CSCI019\', \'Bob\', \'path\', \'I am a book hi\', '+ d.getTime()/1000 + ')');
 
 
 }
@@ -30,4 +33,4 @@ conn.query('INSERT INTO books (title, author, class, seller, image, time) VALUES
 
 
 conn.query('INSERT INTO users (email, password, name) VALUES (\'wheels@nyc.rr.com\', \'test\', \'Max Lesser\')');
-conn.query('INSERT INTO users (email, password, name) VALUES (\'andy@nyc.rr.com\', \'test\', \'Andy Lesser\')');
+conn.query('INSERT INTO users (email, password, name) VALUES (\'andy@nyc.rr.com\', \'test\', \'Andy Lesser\')');conn.query('INSERT INTO users (email, password, name) VALUES (\'andy@nyc.rr.com\', \'test\', \'Andy Lesser\')');
