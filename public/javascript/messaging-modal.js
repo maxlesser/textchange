bfbce38988f1d1b8251905bb586e8fb13dd0e402
Jon <jmillstein1@gmail.com>
2013-05-05 14:57:24 -0400

    Messaging modal with tabs

--- public/javascript/messaging-modal.js
+++ public/javascript/messaging-modal.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$("#messaging-button").click(function () {
-		$('#myModal').modal('toggle');
+		$('#messaging-modal').modal('toggle');
 		console.log("Messaging button pressed");
 	});
 });
